fe340c8014c181afc04ca509954526e7937d36aa
julisbf <bfjuliana@gmail.com>
2019-02-13 12:32:27 -0500

    Create FooterJim.vue

---
+++ src/components/FooterJim.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer role="contentinfo">
+    <a href="8ballcommunity.club" target="_blank" rel="noopener">8ballcommunity.club</a>
+    - ©2019 8 BALL COMMUNITY All rights reserved
+  </footer>
+</template>
+<style lang="scss" scoped>
+footer {
+  border-top: 2px dashed $secondary-color;
+  margin: 32px 0 16px;
+  padding: 16px 0;
+}
+</style>
