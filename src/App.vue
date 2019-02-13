f47c685cc829d95a2d77117b29ebddfa10aa2a75
julisbf <bfjuliana@gmail.com>
2019-02-13 12:31:05 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HeaderJim></HeaderJim>
+    <main class="main" role="main">
+      <Hero></Hero>
+      <Intro></Intro>
+      <Info></Info>
+    </main>
+    <FooterJim></FooterJim>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HeaderJim from "./components/HeaderJim.vue";
+import Hero from "./components/Hero.vue";
+import Intro from "./components/Intro.vue";
+import Info from "./components/Info.vue";
+import FooterJim from "./components/FooterJim.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HeaderJim,
+    Hero,
+    Intro,
+    Info,
+    FooterJim
   }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: $text-primary-color;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 100%;
+  font-weight: 400;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-rendering: optimizeLegibility;
+  -webkit-font-kerning: normal;
+  font-kerning: normal;
+  -webkit-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  text-size-adjust: 100%;
 }
 </style>
