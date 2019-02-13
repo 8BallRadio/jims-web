81bbd257ffd8587a6d16aeac22a2470f74b1ff3a
julisbf <bfjuliana@gmail.com>
2019-02-13 12:31:50 -0500

    Create Hero.vue

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="spiderweb" id="spiderweb">
+    <img
+      src="../assets/spiderweb.png"
+      alt="Spiderweb illustration"
+      srcset="../assets/spiderwebtiny.png 100w, ../assets/spiderweb.png 500w, ../assets/spiderwebbig.png 2x"
+    >
+  </section>
+</template>
+<style lang="scss" scoped>
+.spiderweb {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.spiderweb img {
+  display: block;
+  width: 80vmin;
+}
+</style>
