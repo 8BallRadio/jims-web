62a12ace401e8e5fc542fc8b3ca1e76cdc4f017f
julisbf <bfjuliana@gmail.com>
2019-02-13 19:32:01 -0500

    Add picture tag

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,20 @@
 <template>
   <section class="spiderweb" id="spiderweb">
-    <img
-      src="../assets/spiderweb.png"
-      alt="Spiderweb illustration"
-      srcset="../assets/spiderwebtiny.png 100w, ../assets/spiderweb.png 500w, ../assets/spiderwebbig.png 2x"
-    />
+    <picture>
+      <source
+        media="(min-width: 767px)"
+        srcset="../assets/spiderweb.png, ../assets/spiderweb-2x.png 2x"
+      >
+      <source
+        media="(min-width: 479px)"
+        srcset="../assets/spiderwebtiny.png, ../assets/spiderwebtiny-2x.png 2x"
+      >
+      <img
+        src="../assets/spiderweb-fb.png"
+        alt="Spiderweb illustration, created with black spots"
+        srcset="../assets/spiderweb-fb-2x.png 2x"
+      >
+    </picture>
   </section>
 </template>
 <style lang="scss" scoped>
