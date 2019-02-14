e34eb2cfda62ee2a788dac68d71659ef83636bdc
julisbf <bfjuliana@gmail.com>
2019-02-14 17:15:54 -0500

    Remove fixed top bar during scrolling

--- src/components/HeaderJim.vue
+++ src/components/HeaderJim.vue
@@ -1,65 +1,8 @@
 <template>
-  <header class="header" :class="positionFixed" role="banner">
+  <header class="header header--fixed" role="banner">
     <h1 class="header__text">JIM'S WEB</h1>
   </header>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      positionFixed: ""
-    };
-  },
-  methods: {
-    handleScroll: function() {
-      const header = document.querySelector(".header");
-      const windowHeight = window.innerHeight;
-      let position = this.getPosition(header);
-
-      if (Math.abs(position.y) >= windowHeight / 2) {
-        if (!header.classList.contains("header--fixed")) {
-          this.positionFixed = "header--fixed";
-        }
-      } else {
-        if (header.classList.contains("header--fixed")) {
-          this.positionFixed = "";
-        }
-      }
-    },
-    getPosition: function(el) {
-      let xPos = 0;
-      let yPos = 0;
-
-      while (el) {
-        if (el.tagName == "BODY") {
-          // deal with browser quirks with body/window/document and page scroll
-          let xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-          let yScroll = el.scrollTop || document.documentElement.scrollTop;
-
-          xPos += el.offsetLeft - xScroll + el.clientLeft;
-          yPos += el.offsetTop - yScroll + el.clientTop;
-        } else {
-          // for all other non-BODY elements
-          xPos += el.offsetLeft - el.scrollLeft + el.clientLeft;
-          yPos += el.offsetTop - el.scrollTop + el.clientTop;
-        }
-
-        el = el.offsetParent;
-      }
-      return {
-        x: xPos,
-        y: yPos
-      };
-    }
-  },
-  created: function() {
-    window.addEventListener("scroll", this.handleScroll);
-  },
-  destroyed: function() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
-};
-</script>
 <style lang="scss" scoped>
 .header {
   border-bottom: 2px dashed $secondary-color;
