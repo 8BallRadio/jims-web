b358e5b55043ea87625995154a7cf694980b1634
julisbf <bfjuliana@gmail.com>
2019-02-14 17:18:10 -0500

    Apply linting

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,16 +4,16 @@
       <source
         media="(min-width: 767px)"
         srcset="../assets/spiderweb.png, ../assets/spiderweb-2x.png 2x"
-      >
+      />
       <source
         media="(min-width: 479px)"
         srcset="../assets/spiderwebtiny.png, ../assets/spiderwebtiny-2x.png 2x"
-      >
+      />
       <img
         src="../assets/spiderweb-fb.png"
         alt="Spiderweb illustration, created with black spots"
         srcset="../assets/spiderweb-fb-2x.png 2x"
-      >
+      />
     </picture>
   </section>
 </template>
