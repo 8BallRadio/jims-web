b285f543092afba97e255f59c63bdb0731041dcd
julisbf <bfjuliana@gmail.com>
2019-02-19 14:10:09 -0500

    Add 'How to Apply' button

--- src/components/HeaderJim.vue
+++ src/components/HeaderJim.vue
@@ -1,8 +1,20 @@
 <template>
   <header class="header header--fixed" role="banner">
     <h1 class="header__text">JIM'S WEB</h1>
+    <button class="btn btn__how-to" @click="handleClick">HOW TO APPLY</button>
   </header>
 </template>
+<script>
+export default {
+  name: "HeaderJim",
+  methods: {
+    handleClick: function() {
+      this.$root.$emit("clickedHowTo");
+    }
+  }
+};
+</script>
+
 <style lang="scss" scoped>
 .header {
   border-bottom: 2px dashed $secondary-color;
@@ -21,5 +33,31 @@
 .header__text {
   color: $secondary-color;
   margin-bottom: 8px;
+  margin-top: 16px;
+  float: left;
+}
+
+.btn {
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  font-family: "Courier New", Courier, monospace;
+  font-weight: bold;
+}
+
+.btn__how-to {
+  float: right;
+  font-size: 0.875rem;
+  margin-top: 20px;
+  padding-right: 0;
+
+  &:hover,
+  &:active {
+    color: $secondary-color;
+  }
+
+  @media (min-width: 479px) {
+    font-size: 1rem;
+  }
 }
 </style>
